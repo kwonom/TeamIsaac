f5e27dc83a86457944f872c83f3ccecc80db2f9a
kooyh <kooyh711@naver.com>
2023-01-20 10:25:05 +0900

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Input.mousePosition;
+        }
     }
 }
