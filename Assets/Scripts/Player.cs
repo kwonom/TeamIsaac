312b9eef84f5b011a93ac7b3e41ef010ff28bddb
DESKTOP-8VJVGPD\admin <kwonom>
2023-01-20 14:28:39 +0900

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    GameObject _bullet;
-
-    int _hp = 10;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -19,17 +16,6 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             transform.position = Input.mousePosition;
-        }               
-
-        if(Input.GetKey(KeyCode.Space))
-        {
-
-        }
-
-        if(_hp<=0)
-        {
-            GetComponent<ScenesChange>().Dead();
         }
     }
-   
 }
