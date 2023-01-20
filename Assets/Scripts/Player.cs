371c3671daeaefeed07a1e5a1f1f38bba7bc4811
DESKTOP-8VJVGPD\admin <kyh>
2023-01-20 15:31:53 +0900

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator _ani;
+    bool isIdle = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _ani = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        Attack();
+
+        if (isIdle)
         {
-            transform.position = Input.mousePosition;
+            _ani.SetInteger("Attack", 0);
+
+
         }
+        if (Input.GetKey("d"))
+        {
+           
+        }
+
+
+
     }
+
+    void move()
+    {
+       
+    }
+
+    void Attack()
+    {
+        
+        if (isIdle)
+        {
+            _ani.SetInteger("Attack", 0);
+
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _ani.SetInteger("Attack", 1);
+            isIdle = false;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _ani.SetInteger("Attack", 1);
+
+            isIdle = false;
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isIdle= true;
+        }
+
+
+    }   
+
 }
