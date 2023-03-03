42ff6ffd829dc512cb8b58a6b8ff8be0458f5a11
DESKTOP-8VJVGPD\admin <kwonom>
2023-03-03 13:43:20 +0900

    재커밋

--- Assets/Scripts/Monster/Horf.cs
+++ Assets/Scripts/Monster/Horf.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Horf : MonoBehaviour
 {
+    [SerializeField] GameObject _horfBullet;
+    [SerializeField] Transform _target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 30;
     }
 
+    private void Awake()
+    {
+        _horfBullet = Resources.Load("Prefabs/HorfBullet") as GameObject;
+        StartCoroutine();
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Horf"), LayerMask.NameToLayer("HorfBullet"));
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator CoHorfFire()
+    {
+        while(true)
+        {
+            GameObject temp = Instantiate(_horfBullet, transform.position, transform.rotation);
+            temp.transform.position = transform.position;
+            temp.GetComponent<HorfBullet>().Init(_target);
+            yield return new WaitForSeconds(3);
+        }
     }
 }
