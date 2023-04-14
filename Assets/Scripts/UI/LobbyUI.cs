7b42ff35ffeb7e946d7cd4729f12baa4b869ccaf
kooyh <kooyh711@naver.com>
2023-04-14 11:06:11 +0900

    LobbyUI

---
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyUI : MonoBehaviour
+{
+    public GameObject _second;
+    [SerializeField] RectTransform _move;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+
+    }
+}
